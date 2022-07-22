47c1313e316bea4e0afddd679aa87dcf5bc02f04
hakim220619 <danilukman2206@gmail.com>
2022-07-22 09:36:02 +0700

    dani

--- application/models/Mod_admin.php
+++ application/models/Mod_admin.php
@@ -27,7 +27,7 @@ class Mod_admin extends CI_Model
         from siswa s
         left join tbl_user tu
         on s.no_pendaftaran=tu.id_user
-        where s.id_verivikasi = '2' and '3'
+        where s.id_verivikasi != '1'
         order by s.id_verivikasi desc
         ");
         return $query;
@@ -39,7 +39,7 @@ class Mod_admin extends CI_Model
         from siswa s
         left join tbl_user tu
         on s.no_pendaftaran=tu.id_user
-        where s.id_verivikasi = '1'
+        where s.id_verivikasi = '1' 
         order by s.id_verivikasi desc
         ");
         return $query;
